a2249219cc335a5d5adfd714d532679d1b568fdf
Mike Lee <mike@caecae.com>
2020-01-27 22:16:33 -0800

    feat: add components and corresponding route
    * ng generate component homepage/home
    * ng generate component homepage/about
    * update the homepage module's route to show the components

--- angular/src/app/homepage/homepage.module.ts
+++ angular/src/app/homepage/homepage.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { HomepageRoutingModule } from './homepage-routing.module';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [HomeComponent, AboutComponent],
   imports: [
     CommonModule,
     HomepageRoutingModule
